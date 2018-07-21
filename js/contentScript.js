97ee37bfa611b6a5a8f74cfa3998d1cb3ee5bc39
shreif <shreif@zeem.sa>
2018-07-21 23:32:05 +0300

    add better support for varaity of browsers

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,13 +1,112 @@
-chrome.runtime.sendMessage({action: "showPageAction"})
+// $(window).load(function () {
+//     console.log('loaded')
+// })
+
+// chrome.runtime.sendMessage({action: "showPageAction"})
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if( request.action == 'getUdemyLink' ) {
-        var src = $('video').attr('src')
-        sendResponse({payload: src})
-    }
-    else if( request.action == "checkVideoExist" ) {
-        sendResponse({exists: $('video').length})
+    if( request.action == "checkVideoExist" ) {
+        // check if youtube
+        if( parseYoutubeId(window.location.href) && window.location.href.includes('youtube') ) {
+            $('.ytp-play-button')[0].click()
+            sendResponse({exists: true, type: "youtube", url: parseUrl(window.location.href) })
+        }
+        else {
+            var type = 'videos';
+            var srcs = getVideoSources();
+            // if no video srcs returned
+            if( srcs.length == 0) {
+                srcs = getIframeSources();
+                type = 'iframes';
+            }
+
+            if( srcs.length == 0 )
+                sendResponse({exists: false})
+            else
+                sendResponse({exists: true, type: type, urls: srcs})
+        }
+        
+        sendResponse({exists: false})
         return true
     }
 
+    sendResponse({exists: false})
+    
 })
+
+var getVideoSources = function () {
+    var videos = checkVideoTag()
+    return getSources(videos);
+}
+
+var getIframeSources = function () {
+    var iframes = checkIframes()
+    return getSources(iframes)
+}
+
+var getSources = function (loop) {
+    var srcs = new Array();
+    $.each(loop, function (v, i) {
+        var src = $(this).attr('src');
+        if( src && (src.includes('video') || src.includes('embed') || src.includes('udemy') || src.includes('shahid')) && $.inArray(src, srcs) == -1 )
+            srcs.push(src) 
+    })
+
+    return srcs;
+}
+
+var checkIframes = function () {
+    return document.getElementsByTagName('iframe');
+}
+
+var checkVideoTag = function () {
+    return document.getElementsByTagName('video');
+}
+
+// parse video id from url
+var parseYoutubeId = function (url) {
+    // http://stackoverflow.com/a/14701040
+    var match = /^.*(youtube.com\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/.exec(url);
+
+    if (match instanceof Array && match[2] !== undefined) {
+        return match[2];
+    } else {
+        return false;
+    }
+}
+
+var parseUrl = function (url) {
+    var videoId = parseYoutubeId(url),
+        listId = getUrlVars(url, 'list'),
+        embedUrl = '';
+
+    if (videoId) {
+        embedUrl = window.location.protocol+'//www.youtube.com/embed/' + videoId;
+
+        if (typeof listId !== 'undefined') {
+            embedUrl += '?list=' + listId;
+        } else {
+            // embedUrl += 'autoplay=1';
+
+            if (typeof time !== 'undefined' && time !== false) {
+                embedUrl += '?start=' + time;
+            }
+        }
+
+        return embedUrl;
+    } else {
+        return false;
+    }
+}
+
+var getUrlVars = function (url, variable) {
+    var vars = {},
+        hash,
+        hashes = url.slice(url.indexOf('?') + 1).split('&');
+
+    for (var i = 0, len = hashes.length; i < len; i += 1) {
+        hash = hashes[i].split('=');
+        vars[hash[0]] = hash[1];
+    }
+    return vars[variable];
+}
--- js/popup.js
+++ js/popup.js
@@ -1,63 +1,94 @@
 $(function () {
+    var extId = 'ponogadffpmjilenhjppjahhbglmagon'
     $(document).ready(function () {
-        var extId = 'ponogadffpmjilenhjppjahhbglmagon'
-        
+
         chrome.storage.sync.get(["appId"], function (result) {
-            if(result.appId)
+            if (result.appId)
                 extId = result.appId
         })
 
         $('#form').on('submit', function (e) {
             e.preventDefault()
 
+            showLoadingText();
+            /**
+             * TODO: List todo
+             * 1- Check if the website contain any video tags, iframes or is youtube
+             * 2- if the website contain multiple tags display them to the user to choose from 
+             * 3- display popup application per selection
+             */
             chrome.tabs.query({
                 active: true,
                 currentWindow: true
             }, function (tabs) {
-                chrome.cookies.getAll({
-                    url: tabs[0].url
-                }, function(cookies) {
-                    console.log(cookies)
-                })
-                // check if page has video
                 chrome.tabs.sendMessage(tabs[0].id, {
                     action: "checkVideoExist"
                 }, function (response) {
-
-                    // return true if page contains video tag
-                    if (response.exists) {
-                        // check if the url is youtube or udemy
-                        if(checkUrl(tabs[0].url) == 'udemy') {
-                            chrome.tabs.sendMessage(tabs[0].id, {
-                                action: "getUdemyLink"
-                            }, function (response) {
+                    console.log(response)
+                    if( response && response.exists ) {
+                        switch(response.type) {
+                            case "youtube":
                                 chrome.runtime.sendMessage(extId, {
-                                    action: "showUdemy",
-                                    url: tabs[0].url
-                                    // url: response.payload
+                                    action: "showVideo",
+                                    url: response.url
                                 }, handleUnloadedExt)
-                            })
-                        } else {
-                            chrome.runtime.sendMessage(extId, {
-                                action: "showYoutube",
-                                url: tabs[0].url
-                            }, handleUnloadedExt)
+                                break;
+
+                            case "iframes":
+                                case "videos":
+                                drawLinks(response.urls);
+                                break;
                         }
+
                     }
-                });
+                    else {
+                        showNoLinks();
+                    }
+                })
             })
         })
     })
 
-    var handleUnloadedExt = function (response) {
-        if( typeof response == 'undefined' )
-            $('label').text('Please load tubepin app or check id')
+    var showLoadingText = function () {
+        $('.form-control').find('label').text('Loading .... !');
     }
 
-    var checkUrl = function (url) {
-        if (url.includes('udemy'))
-            return 'udemy';
-        else
-            return 'youtube';
+    var showNoLinks = function () {
+        $('.form-control').find('label').text('No videos links found!');
+    }
+
+    var showLinksText = function () {
+        $('.form-control').find('label').text('found some links please choose one');
+    }
+
+    var showYoutubeMsg = function () {
+        $('.form-control').find('label').text('Youtube found and video currently loaded');
+    }
+
+    var drawLinks = function (loop) {
+        $('.form-control').prepend('<div class="list list-group"></div>')
+        $.each(loop, function(i, v) {
+            $('.list').append('<a class="links list-group-item list-group-item-action" href="'+v+'">'+_.truncate(v)+'</a>')
+        })
+        iframeLinkClick()
+        showLinksText();
+        $('body').css({'width': '400px', 'height': '400px'})
+    }
+
+    var iframeLinkClick = function () {
+        $('.links').on('click', function (e) {
+            e.preventDefault();
+            chrome.runtime.sendMessage(extId, {
+                action: "showVideo",
+                url: $(this).attr('href')
+            }, handleUnloadedExt)
+        })
+    }
+
+    var handleUnloadedExt = function (response) {
+        if (typeof response == 'undefined')
+            $('label').text('Please load tubepin app or check id')
+        else 
+            showYoutubeMsg();
     }
 })
